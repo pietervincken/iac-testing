b44d3753ec139e305325da53afa9ca92c34bee3a
Pieter Vincken <4153479+pietervincken@users.noreply.github.com>
2026-03-21 14:12:26 +0100

    feat: tested moto and working except test 31

--- 30-orchestration-apply.sh
+++ 30-orchestration-apply.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 cd demo/3-orchestration-repo
-tflocal init
+tflocal init -reconfigure
 tflocal apply --auto-approve
--- 31-orchestration-test.sh
+++ 31-orchestration-test.sh
@@ -1,6 +1,4 @@
 #/bin/bash
 
-cd demo/3-orchestration-repo
-cd end-to-end-tests
-tflocal init
+cd demo/3-orchestration-repo/end-to-end-tests
 ADDITIONAL_TF_OVERRIDE_LOCATIONS=./create-s3-object,./validate-s3-object tflocal test
